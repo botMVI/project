300c72587ce8d4a9cc61e4606b630848d85ceaf3
MVI <example@gmail.com>
2020-04-09 15:39:38 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+
+// console.log();
+
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+let person = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(person["name"]);
+
+let arr = ['plum.png', 'orange.jpeg', 'apple.bmp'];
+
+console.log(arr[0]);
+
+// alert("Hello World");
+
+// let answer = confirm("Are you here?");
+
+// console.log(answer);
+
+// let answer = prompt("Are you under 18?", "Yes");
+
+console.log(typeof(null));
+
+console.log("arr" + " - object");
+console.log(4 + " - object");
