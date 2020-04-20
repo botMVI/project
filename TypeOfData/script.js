efc3dd2e4e92a607dfcd86c256907319fec5e0b5
MVI <example@gmail.com>
2020-04-20 11:14:30 +0600

    TypeDataWork

---
+++ TypeOfData/script.js
@@ -0,0 +1,42 @@
+// // - To String
+
+// // 1)
+// console.log(typeof(String(null)));
+// // 2)
+// console.log('ww' + "err");
+// console.log("https://vk.com/catalog/" + 5);
+
+// // - Number
+
+// // 1)
+// console.log(typeof(Number('5')));
+// // 2)
+// console.log(typeof(5 + +'5'));
+// // 3)
+// console.log(typeof(parseInt('15px', 10)));
+
+// let ans = prompt("Hello?", '');
+
+// // 0, '', null, undefined, NaN
+
+// let switcher = null;
+
+// if(switcher){
+//     console.log("Working..");
+// }
+
+// switcher = 1;
+
+// if(switcher) {
+//     console.log("Working..");
+// }
+
+// // 2)
+// console.log(typeof(Boolean('5')));
+
+// // 3)
+// console.log(typeof((!!'5')));
+
+let x = 5;
+alert( x++ );
+
