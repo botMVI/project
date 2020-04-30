cf9c8ecfe033400c3298c404911663151c12ba33
MVI <example@gmail.com>
2020-04-30 19:49:00 +0600

    addFromWork

--- yoga2/js/script.js
+++ yoga2/js/script.js
@@ -36,7 +36,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
    //Timer
 
-   let deadLine = '2020-04-29 21:00';
+   let deadLine = '2020-05-30 11:26';
 
    function getTimeRemaining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date()),
@@ -62,12 +62,21 @@ window.addEventListener('DOMContentLoaded', function() {
 
     function updateClock() {
 		let t = getTimeRemaining(endtime);
-			hours.textContent = t.hours;
-			minutes.textContent = t.minutes;
-			seconds.textContent = t.seconds;
+
+		function addZero(num) {
+			if (num < 10) {
+				return '0' + num;
+			} else return num;
+		}
+			hours.textContent = addZero(t.hours);
+			minutes.textContent = addZero(t.minutes);
+			seconds.textContent = addZero(t.seconds);
 
 			if (t.total <= 0){
 				clearInterval(timeInterval);
+				hours.textContent = '00';
+				minutes.textContent = '00';
+				seconds.textContent = '00';
             }
     
 }
